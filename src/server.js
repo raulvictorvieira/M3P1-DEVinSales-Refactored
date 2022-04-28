2865e340ae0659b8105f567d3fa4565fbc4a98a8
raulvictorvieira <raulvictorsv@gmail.com>
2022-04-28 02:27:12 -0300

    include server database require

--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3333
 const routes = require('./routes')
+require('./database')
 const swaggerUI = require('swagger-ui-express')
 const swaggerFile = require('./swagger.json')
-const Logger = require('./config/logger')
 const morgan = require('./config/morgan')
+const Logger = require('./config/logger')
 
 app.use(express.json())
 app.use(morgan)
 app.use(routes)
 app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
-app.listen(PORT, () => Logger.info(`Executando na porta ${PORT}`));
+app.listen(PORT, () => Logger.info(`Server running on port ${PORT}`));
